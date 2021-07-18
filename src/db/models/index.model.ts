ec91d2637f294229c0081150a301f196c820f951
Joe Ballantyne <josephpballantyne@gmail.com>
2021-07-18 20:14:34 +0800

    reject unath db

--- src/db/models/index.model.ts
+++ src/db/models/index.model.ts
@@ -28,7 +28,10 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
-    ssl: true,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 });
 
