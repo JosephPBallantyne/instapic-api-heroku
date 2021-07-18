28b400c37ac4b14b7cc87bff543960b809a69ef0
Joe Ballantyne <josephpballantyne@gmail.com>
2021-07-18 16:07:37 +0800

    no dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import path from 'path';
+// import path from 'path';
 import App from './app';
 import { AuthRoute, ImageRoute, DefaultRoute } from './routes';
 
-require('dotenv').config({ path: path.resolve(`${__dirname}/../.env`) });
+// require('dotenv').config({ path: path.resolve(`${__dirname}/../.env`) });
 
 const app = new App([new AuthRoute(), new ImageRoute(), new DefaultRoute()]);
 
